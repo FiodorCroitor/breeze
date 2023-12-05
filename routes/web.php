67b67bac35d398e28e14a5a73b961d5b3acabb51
DESKTOP-ATGVCFJ\user <nokia4415223@gmail.com>
2023-12-05 14:33:53 +0200

    Update project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AgentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +28,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware('auth','role:admin')->group(function (){
+
+    Route::get('/admin/dashboard',[AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+});
+
+Route::middleware('auth', 'role:agent')->group(function (){
+    Route::get('/agent/dashboard',[AgentController::class , 'AgentDashboard'])->name('agent.dashboard');
+});
+
+
